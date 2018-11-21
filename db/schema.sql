493f08b9b67a4aafef5456ce078aaf465dde3ca6
Bobby Boyd <bboyd2008@gmail.com>
2018-11-21 10:58:39 -0800

    Initial

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+
+  id INT AUTO_INCREMENT,
+  burger_name VARCHAR(100) NOT NULL,
+  devoured BOOLEAN NOT NULL,
+  PRIMARY_KEY (id)
+
+)
---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burgers (burger_name, devoured);
+  VALUES 
+    ("cheese burger", false)
+INSERT INTO burgers (burger_name, devoured);
+  VALUES 
+    ("bacon burger", true)
+INSERT INTO burgers (burger_name, devoured);
+  VALUES 
+    ("bbq burger", false)
