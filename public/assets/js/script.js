9870957a3f7aa3ee0a63ebbceda43dc989ad02ef
Bobby Boyd <bboyd2008@gmail.com>
2018-12-02 18:59:14 -0800

    added js file

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -2,6 +2,7 @@ $(document).ready(() => {
   $('.devour').on('submit', (event) => {
     event.preventDefault();
     const burger_id = $(this).children('.burger_id').val();
+    console.log(burger_id);
     $.ajax({
       method: 'PUT',
       url: `/${burger_id}`,
